42176ce37c9aea0e2d0638c98a9e282ddd8b3207
kaiqui <kaiqui82@gmail.com>
2023-04-30 06:31:05 -0300

    fix env collect

--- app/export/env.py
+++ app/export/env.py
@@ -5,5 +5,4 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-DB_CONN = os.environ.get("DB_CONN")
 VAULT_TOKEN = os.environ.get("VAULT_TOKEN")
